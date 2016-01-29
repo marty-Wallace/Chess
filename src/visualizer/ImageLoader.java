30729df5faf4d05367fa26514f5b0860ae046e9c
Martin Wallace <marty.wallace@ymail.com>
2016-01-29 01:31:24 -0800

    Commit test
    I LOVE VIM

--- src/visualizer/ImageLoader.java
+++ src/visualizer/ImageLoader.java
@@ -1,5 +1,15 @@
 package visualizer;
 
+/**
+ * 
+ * @author Martin Wallace
+ * marty.wallace@ymail.com
+ * github.com/marty-Wallace/
+ * 
+ */
 public class ImageLoader {
 
+	public ImageLoader(){
+		
+	}
 }
