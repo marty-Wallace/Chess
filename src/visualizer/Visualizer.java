81b4bc211017a981edcf2e1783cc5988f517a4b8
Martin Wallace <marty.wallace@ymail.com>
2016-02-02 19:19:07 -0800

    More documentation

--- src/visualizer/Visualizer.java
+++ src/visualizer/Visualizer.java
@@ -11,7 +11,12 @@ import javax.swing.JFrame;
 
 import board.Board;
 import player.Player;
-
+/**
+ * Visualizer class to show chess game as a JFrame
+ * @author Martin Wallace
+ * <p> Martin.V.Wallace@ieee.org
+ *
+ */
 public class Visualizer extends Canvas implements Runnable, MouseListener{
 
 	private JFrame frame;
@@ -20,7 +25,11 @@ public class Visualizer extends Canvas implements Runnable, MouseListener{
 	private Image[]whitePieces, blackPieces;
 	private boolean running;
 	private int[][] board;
-	public Visualizer(){
+	
+	/**
+	 * No args constructor for Visualizer
+	 */
+	public Visualizer( ){ 				
 		frame = new JFrame("Marty's Chess Game");
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,22 +40,27 @@ public class Visualizer extends Canvas implements Runnable, MouseListener{
 		this.start();
 	}
 
+	/**
+	 * Starts new thread for this Frame. 
+	 */
 	public void start(){
 		Thread t = new Thread(this);
 		t.setPriority(Thread.MAX_PRIORITY);
 		t.start();
 	}
 
-
+	/**
+	 * set to running and request focus
+	 */
 	public void init() {
 		this.running = true;
 		requestFocus();
-
 		frame.addMouseListener(this);
-
 	}
 
-
+	/**
+	 * main Visualizer loop 
+	 */
 	@Override
 	public void run() {
 		while(running){
@@ -71,12 +85,23 @@ public class Visualizer extends Canvas implements Runnable, MouseListener{
 			
 		
 	}
-
-	public void update(Board boardObject){
+	
+	/**
+	 * Updates board to match game state
+	 * 
+	 * @param boardObject
+	 * the current board 
+	 */
+	public synchronized void update(Board boardObject){
 		this.board = boardObject.getBoardAsIntArray();
 		
 	}
 
+	/**
+	 * Draws game 
+	 * @param g
+	 * Graphics 2D g
+	 */
 	public void render(Graphics2D g){
 		g.drawImage(background, 0, 0, WIDTH, HEIGHT, null);
 
@@ -111,10 +136,19 @@ public class Visualizer extends Canvas implements Runnable, MouseListener{
 
 	}
 	
+	
+	/**
+	 * Displays winner to the screen
+	 * @param p
+	 */
 	public void displayWinner(Player p){
 		
 	}
 	
+	
+	/**
+	 * Finish thread
+	 */
 	public void finish(){
 		this.running = false;
 	}
